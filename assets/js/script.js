de7b9213aa034d215ab30f8781a67ab7c0f1d045
savbennett5 <76716614+savbennett8@users.noreply.github.com>
2021-03-02 21:40:05 -0700

    forecast cards & more styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,53 @@
-//searched city needs to be displayed in 'subtitle' h2 element
 //daily forecast displayed in 'daily-container' div element
 //outlook forecast displayed in 'outlook-container' div element
-
-//today's date needs to be displayed in 'date' span element using moment.js
-let dateEl = document.querySelector("#date");
-let todaysDate = moment().format("MM/DD/YYYY")
-dateEl.textContent = todaysDate;
 
 let searchBtnEl = document.querySelector(".search-btn")
 let cityNameEl = document.querySelector("#city-name");
 let cityInputEl = document.querySelector("#city");
+let city = cityInputEl.value
 let historyList = document.querySelector("#history-list");
+
+//today's date needs to be displayed in 'date' span element using moment.js
+let appendTodaysDate = function() {
+    let dateEl = document.querySelector("#date");
+    let todaysDate = moment().format("MM/DD/YYYY")
+    dateEl.textContent = todaysDate;
+}
 
 //I need a dynamically created list for search history
 let historyMaker = function() {
-    let listItem = document.createElement("li")
-    listItem.addClass("history-list");
-    listItem.textContent(cityInputEl.value);
-    historyList.appendChild(listItem);
+    let listItem = document.createElement("li");
+    listItem.textContent = cityInputEl.value;
 
+    let listButton = document.createElement("button");
+    listButton.className = "history-list";
+    
+    listButton.appendChild(listItem);
+    historyList.appendChild(listButton);
 }
+
+//when history list item is clicked, call weather function
 
 
 //fetch api for daily forecast from openweathermap
-/*let getWeatherToday = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f569c1389ce0a742790e8dd21053476d")
+let getWeatherToday = function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.value + '&appid=f569c1389ce0a742790e8dd21053476d')
     .then(function(city) {
         return city.json();
     })
     .then(function(city) {
-        console.log(data);
+        console.log(city);
     })
-}*/
+}
 
-searchBtnEl.addEventListener("click", function() {
+searchBtnEl.addEventListener("click", function(event) {
+    event.preventDefault();
     //append city name to 'city-name' h2 element at top of page
     cityNameEl.textContent = cityInputEl.value;
+    appendTodaysDate();
     historyMaker();
+    getWeatherToday();
+    cityInputEl.value = '';
 });
 
 /*
